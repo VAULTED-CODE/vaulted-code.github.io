14bd5a03cc643f87916670f4035b51f3786341bb
tomvanmele <tom.v.mele@gmail.com>
2024-09-11 20:20:58 +0200

    slightly better spacing

--- assets/Contact-94ad2afa.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,V as r}from"./VContainer-9fe833fa.js";import{a7 as t,ab as a}from"./index-5417f15b.js";const c={};function e(n,s){return t(),a(r)}const i=o(c,[["render",e]]);export{i as default};
--- assets/Products-93ed11d9.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,V as o}from"./VContainer-9fe833fa.js";import{a7 as c,ab as e}from"./index-5417f15b.js";const t={};function a(n,s){return c(),e(o)}const i=r(t,[["render",a]]);export{i as default};
