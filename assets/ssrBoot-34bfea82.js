7144bb96d5957b9b3a411004457323c072a15ed8
tomvanmele <tom.v.mele@gmail.com>
2024-09-17 16:44:26 +0200

    general update

---
+++ assets/ssrBoot-34bfea82.js
@@ -0,0 +1,1 @@
+import{s,o as t,c as e,aA as n}from"./index-840bd29b.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
