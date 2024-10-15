04a751330817a8ad3fd05d9eb44f236b61a4b165
tomvanmele <tom.v.mele@gmail.com>
2024-10-15 15:25:39 +0200

    slide window margins

---
+++ assets/VSpacer-1064f246.js
@@ -0,0 +1,1 @@
+import{w as a}from"./VAvatar-9099a6ec.js";const r=a("v-spacer","div","VSpacer");export{r as V};
--- assets/VSpacer-b754874f.js
+++
@@ -1,1 +0,0 @@
-import{w as a}from"./VAvatar-32ab5a4d.js";const r=a("v-spacer","div","VSpacer");export{r as V};
