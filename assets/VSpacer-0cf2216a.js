474876ec01f2f0072de4de9384c67ef6bfa39d93
tomvanmele <tom.v.mele@gmail.com>
2024-10-15 16:51:10 +0200

    static assets

---
+++ assets/VSpacer-0cf2216a.js
@@ -0,0 +1,1 @@
+import{w as a}from"./VAvatar-acd6b76d.js";const r=a("v-spacer","div","VSpacer");export{r as V};
--- assets/VSpacer-5ea3af48.js
+++
@@ -1,1 +0,0 @@
-import{w as a}from"./VAvatar-e7541815.js";const r=a("v-spacer","div","VSpacer");export{r as V};
