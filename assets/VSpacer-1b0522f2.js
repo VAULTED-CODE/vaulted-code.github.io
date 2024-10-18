abd269cc080f7024a3c5adeacf67ae92ae9788fc
tomvanmele <tom.v.mele@gmail.com>
2024-10-18 09:44:57 +0200

    better pics

--- assets/VSpacer-1b0522f2.js
+++
@@ -1,1 +0,0 @@
-import"./VRow-a75f7423.js";import{c as r}from"./VAvatar-c8cc1606.js";const e=r("v-spacer","div","VSpacer");export{e as V};
---
+++ assets/VSpacer-f19aa0e4.js
@@ -0,0 +1,1 @@
+import"./VRow-03f1eeb1.js";import{c as r}from"./VAvatar-5c7a07fb.js";const e=r("v-spacer","div","VSpacer");export{e as V};
