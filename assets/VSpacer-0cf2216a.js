834c3fb96745826419535d8da25238fbbd4d0eb3
tomvanmele <tom.v.mele@gmail.com>
2024-10-18 07:40:04 +0200

    text updates and minimal home

--- assets/VSpacer-0cf2216a.js
+++
@@ -1,1 +0,0 @@
-import{w as a}from"./VAvatar-acd6b76d.js";const r=a("v-spacer","div","VSpacer");export{r as V};
---
+++ assets/VSpacer-1b0522f2.js
@@ -0,0 +1,1 @@
+import"./VRow-a75f7423.js";import{c as r}from"./VAvatar-c8cc1606.js";const e=r("v-spacer","div","VSpacer");export{e as V};
