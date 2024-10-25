7ee99fc825da19398cbd463958574c10f85b22fe
tomvanmele <tom.v.mele@gmail.com>
2024-10-25 15:33:26 +0200

    simplified for temp release

---
+++ assets/VSpacer-066da3d4.js
@@ -0,0 +1,1 @@
+import{v as a}from"./VAvatar-f876bce0.js";const r=a("v-spacer","div","VSpacer");export{r as V};
--- assets/VSpacer-3c9d0152.js
+++
@@ -1,1 +0,0 @@
-import"./VRow-2b99e9c0.js";import{c as r}from"./VAvatar-6f3c4eba.js";const e=r("v-spacer","div","VSpacer");export{e as V};
